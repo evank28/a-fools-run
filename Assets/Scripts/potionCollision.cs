72993a88914b7fc2516ad99dc332cbd9ae5acad0
Arya Rafii <aryrafii@amd.com>
2022-01-29 13:55:03 -0500

    Potion will explode on collision with player

---
+++ Assets/Scripts/potionCollision.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potionCollision : MonoBehaviour
+{
+  public GameObject particleEffect;
+  void OnCollisionEnter(Collision collision) {
+    if (collision.collider.CompareTag("Player")) Explode();
+    Debug.Log("Hit the player");
+  }
+
+  void Explode() {
+    GameObject effect = Instantiate(particleEffect,
+                                    transform.position,
+                                    Quaternion.identity);
+    Destroy(gameObject);
+  }
+}

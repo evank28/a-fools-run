e1a1104b85755b0d57aaffdb4f5ef45e8d32dc4f
KeweiQ <kewei.qiu@outlook.com>
2022-02-04 14:37:35 -0500

    merge from dev

--- Assets/Scripts/MovePB.cs
+++ Assets/Scripts/MovePB.cs
@@ -8,20 +8,24 @@ public class MovePB : MonoBehaviour
     private float _rotationInput;
     private Vector3 _userRot;
     private bool _userJumped;
+    private bool _jumpInProgress = false;
 
-    private const float _inputScale = 0.5f;
+
+    private const float InputScale = 0.5f;
+    private const float GroundThreshold = 0;
+    private const float CloseToGroundThreshold = 0.1f;
+    private const float JumpMultiplier = 1.6f;
+    private const float MaxSpeed = 5.0f;
 
     private Rigidbody _rigidbody;
     private Transform _transform;
 
-    // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         _playerInput = Input.GetAxis("Vertical");
@@ -35,12 +39,39 @@ public class MovePB : MonoBehaviour
         _userRot += new Vector3(0, _rotationInput, 0);
 
         _transform.rotation = Quaternion.Euler(_userRot);
-        _rigidbody.velocity += transform.forward * _playerInput * _inputScale;
 
-        if(_userJumped)
-        {
-            _rigidbody.AddForce(Vector3.up, ForceMode.VelocityChange);
-            _userJumped = false;
+        // Up is always y so velocity of x and z is clamped down
+        if (euclideanNorm(_rigidbody.velocity.x,
+                          _rigidbody.velocity.z) < MaxSpeed) {
+          _rigidbody.velocity += transform.forward * _playerInput * InputScale;
         }
+
+        // If the player is *close* to the ground, the jump will be triggered.
+        // This allows for a "harder"/"longer" keypress to enable a slightly larger jump.
+        if(_userJumped && ((_jumpInProgress && _transform.position[1] <= CloseToGroundThreshold) ||  _transform.position[1] <= GroundThreshold))
+        {
+            _rigidbody.AddForce(Vector3.up * JumpMultiplier, ForceMode.Impulse);
+            _userJumped = false;
+            _jumpInProgress = true;
+        }
+
+        // Once the user is far from the ground, indicate a jump is no longer in progress
+        // This will require the user to hit the ground again before jumping again.
+        if (_jumpInProgress && _transform.position[1] > CloseToGroundThreshold)
+        {
+            _jumpInProgress = false;
+        }
+
+        // Once the user is far from the ground, indicate a jump is no longer in progress
+        // This will require the user to hit the ground again before jumping again.
+        if (_jumpInProgress && _transform.position[1] > CloseToGroundThreshold)
+        {
+            _jumpInProgress = false;
+        }
+    }
+
+    /** Return the euclidean norm of x and y */
+    private float euclideanNorm (float x, float y) {
+      return Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2));
     }
 }

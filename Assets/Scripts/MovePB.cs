1af19c477cbc60987b9c0c5f2a53c6b70a00ea73
Arya Rafii <42868263+rafiiary@users.noreply.github.com>
2022-02-04 12:20:02 -0500

    Fix player speed (#15)
    * Fixed spawn of the potion generation to drop in front of the player and at a certain height
    
    * fix: ensure user hits the ground before re-jumping
    
    * Adding the potionBottle and potion exploding upon colission with player
    
    * Potion random generation in front of the player at certain height
    
    * Clamp the player speed down to 5.0f
    
    * Add clamping of speed in x and z directions while keeping y jumping capability
    
    * Fixing comment and adding brackets to if statement for style
    
    Co-authored-by: Arya Rafii <aryrafii@amd.com>
    Co-authored-by: evank28 <evankanter28@gmail.com>

--- Assets/Scripts/MovePB.cs
+++ Assets/Scripts/MovePB.cs
@@ -10,11 +10,13 @@ public class MovePB : MonoBehaviour
     private bool _userJumped;
     private bool _jumpInProgress = false;
 
+
     private const float InputScale = 0.5f;
     private const float GroundThreshold = 0;
     private const float CloseToGroundThreshold = 0.1f;
     private const float JumpMultiplier = 1.6f;
-    
+    private const float MaxSpeed = 5.0f;
+
     private Rigidbody _rigidbody;
     private Transform _transform;
 
@@ -37,8 +39,13 @@ public class MovePB : MonoBehaviour
         _userRot += new Vector3(0, _rotationInput, 0);
 
         _transform.rotation = Quaternion.Euler(_userRot);
-        _rigidbody.velocity += transform.forward * _playerInput * InputScale;
-        
+
+        // Up is always y so velocity of x and z is clamped down
+        if (euclideanNorm(_rigidbody.velocity.x,
+                          _rigidbody.velocity.z) < MaxSpeed) {
+          _rigidbody.velocity += transform.forward * _playerInput * InputScale;
+        }
+
         // If the player is *close* to the ground, the jump will be triggered.
         // This allows for a "harder"/"longer" keypress to enable a slightly larger jump.
         if(_userJumped && ((_jumpInProgress && _transform.position[1] <= CloseToGroundThreshold) ||  _transform.position[1] <= GroundThreshold))
@@ -47,12 +54,24 @@ public class MovePB : MonoBehaviour
             _userJumped = false;
             _jumpInProgress = true;
         }
-        
+
         // Once the user is far from the ground, indicate a jump is no longer in progress
         // This will require the user to hit the ground again before jumping again.
         if (_jumpInProgress && _transform.position[1] > CloseToGroundThreshold)
         {
             _jumpInProgress = false;
         }
+
+        // Once the user is far from the ground, indicate a jump is no longer in progress
+        // This will require the user to hit the ground again before jumping again.
+        if (_jumpInProgress && _transform.position[1] > CloseToGroundThreshold)
+        {
+            _jumpInProgress = false;
+        }
+    }
+
+    /** Return the euclidean norm of x and y */
+    private float euclideanNorm (float x, float y) {
+      return Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2));
     }
 }

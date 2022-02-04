fe7006a5b62fafdc1ec6e6663095f19c7034f9bd
evank28 <evankanter28@gmail.com>
2022-02-03 20:27:22 -0500

    fix: ensure user hits the ground before re-jumping

--- Assets/Scripts/MovePB.cs
+++ Assets/Scripts/MovePB.cs
@@ -8,10 +8,12 @@ public class MovePB : MonoBehaviour
     private float _rotationInput;
     private Vector3 _userRot;
     private bool _userJumped;
+    private bool _jumpInProgress = false;
 
-    private const float _inputScale = 0.5f;
-    private const float _groundThreshold = 0.1f;
-    private const float _jumpMultiplier = 1.6f;
+    private const float InputScale = 0.5f;
+    private const float GroundThreshold = 0;
+    private const float CloseToGroundThreshold = 0.1f;
+    private const float JumpMultiplier = 1.6f;
     
     private Rigidbody _rigidbody;
     private Transform _transform;
@@ -35,14 +37,22 @@ public class MovePB : MonoBehaviour
         _userRot += new Vector3(0, _rotationInput, 0);
 
         _transform.rotation = Quaternion.Euler(_userRot);
-        _rigidbody.velocity += transform.forward * _playerInput * _inputScale;
+        _rigidbody.velocity += transform.forward * _playerInput * InputScale;
         
         // If the player is *close* to the ground, the jump will be triggered.
         // This allows for a "harder"/"longer" keypress to enable a slightly larger jump.
-        if(_userJumped && _transform.position[1] <= _groundThreshold)
+        if(_userJumped && ((_jumpInProgress && _transform.position[1] <= CloseToGroundThreshold) ||  _transform.position[1] <= GroundThreshold))
         {
-            _rigidbody.AddForce(Vector3.up * _jumpMultiplier, ForceMode.Impulse);
+            _rigidbody.AddForce(Vector3.up * JumpMultiplier, ForceMode.Impulse);
             _userJumped = false;
+            _jumpInProgress = true;
+        }
+        
+        // Once the user is far from the ground, indicate a jump is no longer in progress
+        // This will require the user to hit the ground again before jumping again.
+        if (_jumpInProgress && _transform.position[1] > CloseToGroundThreshold)
+        {
+            _jumpInProgress = false;
         }
     }
 }

2273e7948c32ae4c6706cecc9f4de273b536f4b8
Arya Rafii <aryrafii@amd.com>
2022-02-03 19:57:19 -0500

    Potion random generation in front of the player at certain height

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -6,7 +6,7 @@ public class Spawn : MonoBehaviour
 {
   public GameObject spawnObject; // The object to spawn repeatedly
   public float timeToSpawn; // Time in seconds before the next spawn of spawnObject
-  public float xmin, xmax, ymin, ymax, z; // position bounds
+  public float xmin, xmax, y, zmin, zmax; // position bounds
   private Vector3 position; // position to spawn
 
   void Start() {
@@ -14,28 +14,27 @@ public class Spawn : MonoBehaviour
   }
 
   IEnumerator spawn () {
-    while (true) {
-      transform.position = new Vector3 (0,0,0);
 
-      // Create new random position within given parameters
-      position = new Vector3(Random.Range(xmin, xmax),
-                             Random.Range(ymin, ymax),
-                             z);
-      transform.position += position;
+      while(true) {
+        // Set the position of the spawn object as the position of the player
+        position += transform.position;
+        position += transform.forward * 3;
 
-      Debug.Log("Waiting for " + timeToSpawn.ToString() + " seconds.");
-      /** Wait for some time before spawning again.
-      In case of the potion, the time to spawn again
-      should decrease as the player levels up */
-      yield return new WaitForSeconds (timeToSpawn);
 
-      Debug.Log("Instantiating object " + spawnObject.name + " in location "
-                + position.x.ToString() + " "
-                + position.y.ToString() + " "
-                + position.z.ToString());
-      GameObject potion  = Instantiate(spawnObject,
-                                       transform.position,
-                                       Quaternion.identity);
-    }
+        Debug.Log("Instantiating object " + spawnObject.name + " in location "
+                  + position.x.ToString() + " "
+                  + position.y.ToString() + " "
+                  + position.z.ToString());
+
+        GameObject potion  = Instantiate(spawnObject,
+                                         position,
+                                         Quaternion.identity);
+
+        Debug.Log("Waiting for " + timeToSpawn.ToString() + " seconds.");
+        /** Wait for some time before spawning again.
+        In case of the potion, the time to spawn again
+        should decrease as the player levels up */
+        yield return new WaitForSeconds (timeToSpawn);
+      }
   }
 }

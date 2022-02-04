03a39e069c86e4f84b498da9de83e7b17f1734d2
Arya Rafii <aryrafii@amd.com>
2022-02-03 20:05:24 -0500

    Fixed spawn of the potion generation to drop in front of the player and at a certain height

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -6,7 +6,6 @@ public class Spawn : MonoBehaviour
 {
   public GameObject spawnObject; // The object to spawn repeatedly
   public float timeToSpawn; // Time in seconds before the next spawn of spawnObject
-  public float xmin, xmax, y, zmin, zmax; // position bounds
   private Vector3 position; // position to spawn
 
   void Start() {
@@ -17,8 +16,9 @@ public class Spawn : MonoBehaviour
 
       while(true) {
         // Set the position of the spawn object as the position of the player
-        position += transform.position;
+        position = transform.position;
         position += transform.forward * 3;
+        position += transform.up * 7;
 
 
         Debug.Log("Instantiating object " + spawnObject.name + " in location "

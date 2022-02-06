2d6c8dfd69773b28b6951f21511dbd1b15e8e4c9
Arya Rafii <aryrafii@amd.com>
2022-02-05 21:40:07 -0500

    Spawn of the potions works correctly now

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -6,22 +6,30 @@ public class PlayerCollision : MonoBehaviour
 {
   public GameObject character1;
   public GameObject character2;
+
+  void Start()
+  {
+    // Set the active player to player 2 for now for transform of spawn to work
+    transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character2);
+  }
+
   void OnCollisionEnter(Collision collision)
   {
     print($"collision occured with {collision.collider.name}");
     if (collision.collider.CompareTag("Potion")) {
-      // Destroy(collision.collider.gameObject);
       collision.collider.gameObject.GetComponent<potionCollision>().Explode();
       if (character2.activeSelf) {
         character1.transform.position = character2.transform.position;
         character1.transform.rotation = character2.transform.rotation;
         character2.SetActive(false);
         character1.SetActive(true);
+        transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character1);
       } else if (character1.activeSelf) {
         character2.transform.position = character1.transform.position;
         character2.transform.rotation = character1.transform.rotation;
         character1.SetActive(false);
         character2.SetActive(true);
+        transform.parent.gameObject.GetComponent<Spawn>().setActivePlayer(character2);
       }
     }
   }

e2aaace41e716514c8ff8dbc3f4453c182bdbdfe
KeweiQ <kewei.qiu@outlook.com>
2022-02-20 17:28:27 -0500

    fix bug in camera vertical rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -15,18 +15,15 @@ public class CameraController : MonoBehaviour
     public float smoothFactor = 0.5f;
 
     // multiplier for camera rotation
-    public float rotateSpeedX = 3.0f;
-    public float rotateSpeedY = 3.0f;
-
-    // switches related to camera roration
-    public bool lookAtPlayer = true;
-    public bool rotateAroundPlayer = true;
+    public float rotateSpeedH = 3.0f;
+    public float rotateSpeedV = 3.0f;
 
     // initial camera rotation angle
     Quaternion center;
 
     // camera rotaion angle limit
-    public float maxAngle = 0.1f;
+    private float minAngle = -45f;
+    private float maxAngle = 45f;
 
     // Start is called before the first frame update
     void Start() {
@@ -37,31 +34,52 @@ public class CameraController : MonoBehaviour
     }
 
     // LateUpdate is called after Update methods
-    void Update() {
+    void LateUpdate() {
         // let camera follow player
         Vector3 newPosition = PlayerTransform.position + this._cameraOffset;
         transform.position = Vector3.Slerp(transform.position, newPosition, smoothFactor);
 
-        if (rotateAroundPlayer) {
-            // get mouse input can calculate rotation angles
-            Quaternion cameraTurnAngleX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotateSpeedX, Vector3.up);
-            Quaternion cameraTurnAngleY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * rotateSpeedY * -1, Vector3.right);
+        // get mouse input can calculate rotation angles
+        float rotateH = Input.GetAxis("Mouse X") * rotateSpeedH;
+        float rotateV = Input.GetAxis("Mouse Y") * rotateSpeedV * -1;
+        
+        // perform horizontal rotation
+        Quaternion cameraTurnAngleY = Quaternion.AngleAxis(rotateH, Vector3.up);
+        Quaternion cameraTurnAngleZ, cameraTurnAngleX;
+        
+        // calculate vertical rotation on Z axis within limit
+        if (_cameraOffset[2] >= 0) {
+            cameraTurnAngleZ = Quaternion.AngleAxis(rotateV, Vector3.left);
+        } else {
+            cameraTurnAngleZ = Quaternion.AngleAxis(rotateV, Vector3.right);
+        }
+        
+        // calculate vertical rotation on X axis within limit
+        if (_cameraOffset[0] >= 0) {
+            cameraTurnAngleX = Quaternion.AngleAxis(rotateV, Vector3.forward);
+        } else {
+            cameraTurnAngleX = Quaternion.AngleAxis(rotateV, Vector3.back);
+        }
 
-            // perform vertical rotation within limit
-            Quaternion temp = transform.rotation * cameraTurnAngleY;
-            Debug.Log(Quaternion.Angle(this.center, temp));
-            if (Quaternion.Angle(this.center, temp) < this.maxAngle) {
-                this._cameraOffset = cameraTurnAngleY * this._cameraOffset;
-            }
-
-            // perform horizontal rotation
+        // perform vertical rotation on Z axis within limit (bugs in limit need to be fixed)
+        Quaternion tempZ = transform.rotation * cameraTurnAngleZ;
+        //if (Quaternion.Angle(this.center, tempZ) < this.maxAngle) {
+            this._cameraOffset = cameraTurnAngleZ * this._cameraOffset;
+        //}
+        
+        // perform vertical rotation on X axis within limit (bugs in limit need to be fixed)
+        Quaternion tempX = transform.rotation * cameraTurnAngleX;
+        //if (Quaternion.Angle(this.center, tempX) < this.maxAngle) {
             this._cameraOffset = cameraTurnAngleX * this._cameraOffset;
-            this.center = cameraTurnAngleX * this.center;
-        }
+        //}
 
-        // let the camera focus on player
-        if (lookAtPlayer || rotateAroundPlayer) {
-            transform.LookAt(PlayerTransform.position);
-        }
+        // perform horizontal rotation
+        this._cameraOffset = cameraTurnAngleY * this._cameraOffset;
+
+        // update center angle
+        this.center = cameraTurnAngleY * this.center;
+
+        // let camera focus on character
+        this.transform.LookAt(PlayerTransform.position);
     }
 }
